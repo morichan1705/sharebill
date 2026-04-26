313e5fdb07267425bd2186a84b92017b7a928b70
morichan1705 <morichan1705@gmail.com>
2026-04-26 13:26:09 +0700

    debug

--- app.py
+++ app.py
@@ -156,7 +156,7 @@ with tab1:
         if st.button("💾 Cập nhật thông tin bản thân", type="primary"):
             st.session_state.members[nickname] = {"bank": my_bank, "acc": my_acc}
             save_data()
-            st.toast("Đã cập nhật số tài khoản thành công!", icon="✅")
+            st.success("Đã cập nhật số tài khoản thành công!")
             st.rerun()
 
     st.write("---")
@@ -204,13 +204,13 @@ with tab1:
                     if bc1.button("💾 Lưu thay đổi", key=f"save_btn_{f_name}", use_container_width=True):
                         st.session_state.members[f_name] = {"bank": edit_f_bank, "acc": edit_f_acc}
                         save_data()
-                        st.toast(f"Đã cập nhật thông tin của {f_name}!", icon="✅")
+                        st.success(f"Đã cập nhật thông tin của {f_name}!")
                         st.rerun()
                         
                     if bc2.button("🗑️ Xóa bạn", key=f"del_btn_{f_name}", use_container_width=True):
                         st.session_state.members.pop(f_name)
                         save_data()
-                        st.toast(f"Đã xóa {f_name} khỏi danh bạ!", icon="🗑️")
+                        st.success(f"Đã xóa {f_name} khỏi danh bạ!")
                         st.rerun()
 
     st.write("---")
@@ -238,7 +238,7 @@ with tab1:
                 else:
                     st.session_state.groups[new_g_name.strip()] = new_g_members
                     save_data()
-                    st.toast(f"Đã tạo nhóm {new_g_name} thành công!", icon="🎉")
+                    st.success(f"Đã tạo nhóm {new_g_name} thành công!")
                     st.rerun()
 
     with col_list_group:
@@ -260,13 +260,13 @@ with tab1:
                         else:
                             st.session_state.groups[g_name] = edit_g_members
                             save_data()
-                            st.toast(f"Đã cập nhật nhóm {g_name}!", icon="✅")
+                            st.success(f"Đã cập nhật nhóm {g_name}!")
                             st.rerun()
                             
                     if gc2.button("🗑️ Xóa nhóm", key=f"del_g_{g_name}", use_container_width=True):
                         st.session_state.groups.pop(g_name)
                         save_data()
-                        st.toast(f"Đã giải tán nhóm {g_name}!", icon="🧨")
+                        st.success(f"Đã giải tán nhóm {g_name}!")
                         st.rerun()
 # --- TAB 2: GHI HÓA ĐƠN ---
 with tab2:
